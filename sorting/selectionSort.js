fe731f404a55480db5ed675172db680655614d7c
PavlovVasil <vasil@vasilpavlov.com>
2019-12-01 00:39:12 +0200

    Refactor and update comments

--- sorting/selectionSort.js
+++ sorting/selectionSort.js
@@ -5,25 +5,22 @@
  *  2) Remaining subarray which is unsorted.
  * In every iteration of selection sort, the minimum element (considering ascending order) from the unsorted
  * subarray is picked and moved to the sorted subarray.
-*/
+ */
 
 let arr = [3, 5, 1, -22, 0, 201, -10, 84]
 
-const sort = (arr) => { 
+const sort = (arr) => {
     const sortedArr = [...arr];
-    let n = sortedArr.length; 
-    // One by one move boundary of unsorted subarray 
-    for (let i = 0; i < n-1; i++) 
-    { 
-        // Find the minimum element in unsorted array 
-        let min_idx = i; 
-        for (let j = i+1; j < n; j++) 
-            if (sortedArr[j] < sortedArr[min_idx]) 
-                min_idx = j; 
-        debugger
-        // Swap the found minimum element with the first 
-        // element 
-        [sortedArr[i], sortedArr[min_idx]] = [sortedArr[min_idx], sortedArr[i]] 
+    const n = sortedArr.length;
+    // One by one move the boundary of the unsorted subarray
+    for (let i = 0; i < n - 1; i++) {
+        // Find the minimum element in unsorted part of the array 
+        let min_idx = i;
+        for (let j = i + 1; j < n; j++)
+            if (sortedArr[j] < sortedArr[min_idx])
+                min_idx = j;
+        // Swap the found minimum element with the current element 
+        [sortedArr[i], sortedArr[min_idx]] = [sortedArr[min_idx], sortedArr[i]]
     }
     return sortedArr;
 }
